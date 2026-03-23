295c20b24eb9c017b68e61bb7ad70d818c28ae08
LUTTRINGER JEAN-ROMAIN <jr.luttringer@unistra.fr>
2026-03-23 15:34:17 +0100

    Updates

--- _build/assets/mentoring-BZ9B7v6Z.js
+++ _build/assets/mentoring-BZ9B7v6Z.js
@@ -1,4 +1,4 @@
-import{t as o,j as e,b as c,a as $,S as w,F as b}from"./web-CgP7nCAW.js";import{T as C}from"./index-rzb3QVRB.js";import{j as k}from"./js-yaml-mbYHt68G.js";const x=`phdStudents: \r
+import{t as o,j as e,b as u,a as _,S as $,F as b}from"./web-CgP7nCAW.js";import{T as C}from"./index-rzb3QVRB.js";import{j as k}from"./js-yaml-mbYHt68G.js";const x=`phdStudents: \r
   - title: "Towards Eco-Efficient Autonomous Networks: Deep Programmability and Energy-Aware Routing Protocols"\r
     year: 2025-2028\r
     name: "Florent Hardy"\r
@@ -12,6 +12,13 @@ import{t as o,j as e,b as c,a as $,S as w,F as b}from"./web-CgP7nCAW.js";import{
     description: "ANR TSIA Project TURFU"\r
 \r
 master2Students:\r
+  - title: "Study of Gray Failures in Computer Networks : Testbed and Diagnosis"\r
+    year: 2026\r
+    name: "Céleste Kerlogot"\r
+    supervision: "co-supervision"\r
+    description: "Creation of a testbed to study gray failures in computer networks, and development of diagnosis techniques."\r
+\r
+\r
   - title: "Improving energy efficiency of (programmable) networks"\r
     year: 2025\r
     name: "Florent Hardy"\r
@@ -51,4 +58,4 @@ master1Students:\r
     name: "Thomas Alfroy"\r
     description: "Implemented the Best2cop algorithm for computing Delay Constrained, Least Cost paths for Segment Routing."\r
 \r
-bachelorStudents: []`,a=k.load(x);var A=o("<section class=mentoring-section><h2></h2><div class=students-grid>"),M=o("<span class=supervision>(<!>)"),R=o("<div class=student-card><div class=student-header><h3></h3><span class=year-badge></span></div><p class=student-name></p><p>"),F=o('<div class="student-card empty-card"><p>'),D=o("<div class=container><h1>Mentoring</h1><div class=mentoring-container>");function T(){const s=(t,i=[])=>(()=>{var n=A(),u=n.firstChild,h=u.nextSibling;return e(u,t),e(h,(()=>{var v=$(()=>i.length>0);return()=>v()?c(b,{each:i,children:r=>(()=>{var p=R(),d=p.firstChild,g=d.firstChild,S=g.nextSibling,l=d.nextSibling,y=l.nextSibling;return e(g,()=>r.title),e(S,()=>r.year),e(l,()=>r.name,null),e(l,c(w,{get when(){return r.supervision},get children(){var m=M(),_=m.firstChild,f=_.nextSibling;return f.nextSibling,e(m,()=>r.supervision,f),m}}),null),e(y,()=>r.description),p})()}):F()})()),n})();return(()=>{var t=D(),i=t.firstChild,n=i.nextSibling;return e(t,c(C,{children:"Mentoring - Jean-Romain Luttringer"}),i),e(n,()=>s("PhD Students",a.phdStudents),null),e(n,()=>s("Master 2 Students",a.master2Students),null),e(n,()=>s("Master 1 Students",a.master1Students),null),e(n,()=>s("Bachelor Students",a.bachelorStudents),null),t})()}export{T as default};
+bachelorStudents: []`,a=k.load(x);var A=o("<section class=mentoring-section><h2></h2><div class=students-grid>"),M=o("<span class=supervision>(<!>)"),F=o("<div class=student-card><div class=student-header><h3></h3><span class=year-badge></span></div><p class=student-name></p><p>"),R=o('<div class="student-card empty-card"><p>'),D=o("<div class=container><h1>Mentoring</h1><div class=mentoring-container>");function E(){const s=(t,i=[])=>(()=>{var n=A(),c=n.firstChild,h=c.nextSibling;return e(c,t),e(h,(()=>{var v=_(()=>i.length>0);return()=>v()?u(b,{each:i,children:r=>(()=>{var d=F(),p=d.firstChild,g=p.firstChild,y=g.nextSibling,l=p.nextSibling,S=l.nextSibling;return e(g,()=>r.title),e(y,()=>r.year),e(l,()=>r.name,null),e(l,u($,{get when(){return r.supervision},get children(){var m=M(),w=m.firstChild,f=w.nextSibling;return f.nextSibling,e(m,()=>r.supervision,f),m}}),null),e(S,()=>r.description),d})()}):R()})()),n})();return(()=>{var t=D(),i=t.firstChild,n=i.nextSibling;return e(t,u(C,{children:"Mentoring - Jean-Romain Luttringer"}),i),e(n,()=>s("PhD Students",a.phdStudents),null),e(n,()=>s("Master 2 Students",a.master2Students),null),e(n,()=>s("Master 1 Students",a.master1Students),null),e(n,()=>s("Bachelor Students",a.bachelorStudents),null),t})()}export{E as default};
